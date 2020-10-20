b4c16ee6c7d9c679bad70e1301e2388d3f92dcc5
piazzadario <s276010@studenti.polito.it>
2020-10-20 23:41:32 +0200

    server: DB naming convention (all fields lowercase)

--- server/models/queueModel.js
+++ server/models/queueModel.js
@@ -3,7 +3,7 @@ const db = require('./index');
 // TODO - times are not localtime
 exports.createQueue = function() {
     return new Promise ((resolve,reject) =>{
-        const sql = `CREATE TABLE Queue (ID INTEGER NOT NULL PRIMARY KEY, REQUEST_TYPE varchar(5) NOT NULL, COUNTER int, INITIAL_TIME TIMESTAMP NOT NULL DEFAULT (datetime('now','localtime')) , CALLED BOOLEAN NOT NULL CHECK (CALLED IN (0,1)),TIME_SERVED TIMESTAMP, FOREIGN KEY(REQUEST_TYPE) REFERENCES Operations(CODE), FOREIGN KEY(COUNTER) REFERENCES Counters(ID))`;
+        const sql = `CREATE TABLE Queue (id INTEGER NOT NULL PRIMARY KEY, request_type varchar(5) NOT NULL, counter INTEGER, initial_time TIMESTAMP NOT NULL DEFAULT (datetime('now','localtime')) , called BOOLEAN NOT NULL CHECK (called IN (0,1)),time_served TIMESTAMP, FOREIGN KEY(request_type) REFERENCES Operations(CODE), FOREIGN KEY(counter) REFERENCES Counters(id))`;
         db.run(sql,[],(err) =>{
             if(err)
                 reject(err);
@@ -26,7 +26,7 @@ exports.deleteQueue = function() {
 
 exports.addCustomer = function(requestType, numberInQueue, called = 0) {
     return new Promise (function (resolve,reject) {
-        const sql = 'INSERT INTO Queue (REQUEST_TYPE, COUNTER, CALLED) VALUES(?,?,?)'
+        const sql = 'INSERT INTO Queue (request_type, counter, called) VALUES(?,?,?)'
         db.run(sql, [requestType, numberInQueue, called], function(err) {
             if(err)
                 reject(err);
@@ -38,14 +38,15 @@ exports.addCustomer = function(requestType, numberInQueue, called = 0) {
 
 exports.getTicketNumber = function(requestType) {
     return new Promise (function (resolve,reject) {
-        const sql = 'SELECT MAX(COUNTER) AS N FROM Queue WHERE REQUEST_TYPE = ?'
+        const sql = 'SELECT MAX(counter) AS N FROM Queue WHERE request_type = ?'
         db.get(sql, [requestType], function(err, row) {
             if(err)
                 reject(err);
-            if (!row)
+            if (!row['N'])
                 resolve(1);
-            else
+            else{
                 resolve(row["N"]);
+            }
         });
     })
 }
@@ -53,10 +54,10 @@ exports.getTicketNumber = function(requestType) {
 
 exports.getLastCustomers= function(){
     return new Promise ((resolve,reject)=>{
-        const sql=`SELECT ID,COUNTER,TIME_SERVED
+        const sql=`SELECT ID,counter,time_served
                    FROM Queue
-                   WHERE CALLED==1
-                   ORDER BY TIME_SERVED
+                   WHERE called==1
+                   ORDER BY time_served
                    LIMIT 14`
         db.run(sql,(err,results)=>{
             if(err)
