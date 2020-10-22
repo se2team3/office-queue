ff56c603ba4692fb8ecc75af854236e48639576e
Federico Bitondo <bit97@live.it>
2020-10-22 09:14:58 +0200

    Fixed: checkOperation validator

--- server/validators/operationValidator.js
+++ server/validators/operationValidator.js
@@ -5,7 +5,8 @@ const checkOperation = () => {
     return [
         body('code').isAlphanumeric().isLength({min:1, max:5}),
         body('name').isLength({min:1, max:255}),
-        body('description').optional().isLength({min: 0, max: 255})
+        body('description').optional().isLength({min: 0, max: 255}),
+        body('counters').isArray()
     ];
 };
 
